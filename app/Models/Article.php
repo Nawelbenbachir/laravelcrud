8f6a0e52691f09f08a03eaf838d92d8f3e4aef2c
Nawel <benbachirnawel@gmail.com>
2025-06-30 11:59:52 +0200

    ajout table cp_ville

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Cp_Ville extends Model
+class Article extends Model
 {
-    protected $table = 'cp_villes';
+    //
 }
---
+++ database/migrations/2025_06_30_095632_create_articles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->text('description')->nullable();
+            $table->float('prix', 8, 2);
+            $table->timestamp()->onUpdateUseCurrent();
+            $table->timestamp()->onUpdateUseCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
