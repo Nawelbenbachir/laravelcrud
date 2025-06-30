c4018d5532088882d21e0d4f49e4453618aac361
Nawel <benbachirnawel@gmail.com>
2025-06-30 11:48:38 +0200

    ajout table cp_ville

--- database/migrations/2025_06_30_090405_create_cp_villes_table.php
+++ database/migrations/2025_06_30_090405_create_cp_villes_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('cp_villes', function (Blueprint $table) {
             $table->id();
-            $table->string('code_postal')->unique();
-            $table->string('ville');
+            $table->string('code_postal',10);
+            $table->string('ville',100);
+            $table->unique(['code_postal', 'ville']);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
