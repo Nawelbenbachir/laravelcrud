636b82695fd176d8a9ce31cf4c4d926f7a3d9f58
Nawel <benbachirnawel@gmail.com>
2025-06-30 11:08:48 +0200

    ajout table cp_ville

---
+++ app/Models/cp_villes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class cp_villes extends Model
+{
+    //
+}
---
+++ database/migrations/2025_06_30_090405_create_cp_villes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cp_villes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_postal')->unique();
+            $table->string('nom_ville');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cp_villes');
+    }
+};
