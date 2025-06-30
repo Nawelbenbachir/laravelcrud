df7cc5cf89e39b66623599dc36cc4094a8e97a3b
Nawel <benbachirnawel@gmail.com>
2025-06-30 15:05:48 +0200

    ajout code postaux

--- database/seeders/Cp_VilleSeeder.php
+++ database/seeders/Cp_VilleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CodesPostauxSeeder extends Seeder
+class Cp_VilleSeeder extends Seeder
 {
     public function run()
     {
